532c744c5f7f2a7ea62672427d8be08dbe06cc68
nabeelravian01 <116978364+nabeelravian01@users.noreply.github.com>
2022-10-30 01:35:54 +0500

    Update

--- JPEGtoPNGConverter.py
+++ JPEGtoPNGConverter.py
@@ -11,6 +11,7 @@ output_folder = sys.argv[2]
 if not os.path.exists(output_folder):
     os.mkdir(output_folder)
 
+# Loop
 for filename in os.listdir(image_folder):
     img = Image.open(f'{image_folder}{filename}')
     filtered = os.path.splitext(filename)[0]
